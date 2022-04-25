8c5c24efa7cd8bc749516464ab171b4b27f1baae
han_feng <han_feng@foxmail.com>
2022-04-25 13:32:49 +0800

    fix github pages error

--- packages/demo/vite.config.ts
+++ packages/demo/vite.config.ts
@@ -7,6 +7,7 @@ import { defineConfig, splitVendorChunkPlugin } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     vue(),
     splitVendorChunkPlugin(),
